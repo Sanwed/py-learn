dd72b1b214e2d4498eb4ddd2c08c48b929865052
Alexandr Kozyukov <kozyuckov.alex@gmail.com>
2024-07-05 13:28:58 +0400

    solve homowork2 tasks

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homeworks_amount = 12
+hours = 1.5
+course_title = 'Python'
+time_per_task = hours / homeworks_amount
+
+print(f'{course_title}, всего задач:{homeworks_amount}, затрачено часов: {hours}, среднее время выполнения {time_per_task} часа')
