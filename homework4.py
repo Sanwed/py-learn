6fc5194190cb3cb6cffb8033ed0e6a95ccdd74f5
Alexandr Kozyukov <kozyuckov.alex@gmail.com>
2024-07-07 12:37:17 +0400

    solve homework4 tasks

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input('Enter some text: ')
+print('string length:', len(my_string))
+
+print('Upper case:', my_string.upper())
+print('Lower case:', my_string.lower())
+print('No spaces:', my_string.replace(' ', ''))
+print('First symbol:', my_string[0])
+print('Last symbol:', my_string[-1])
