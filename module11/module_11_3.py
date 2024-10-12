528db8bfd6cf4693aabaa5d68c411a2bdb2d008d
Sanwed <kozyuckov.alex@gmail.com>
2024-10-12 19:17:22 +0400

    fix task module_11_3

--- module11/module_11_3.py
+++ module11/module_11_3.py
@@ -1,15 +1,29 @@
+import inspect
 from pprint import pprint
 
 def introspection_info(obj):
-    attrs = dir(obj)
-    methods = [attr for attr in attrs if callable(getattr(obj, attr))]
+    obj_type = type(obj).__name__
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("__")]
+    methods = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith("__")]
+    obj_module = obj.__module__
 
     return {
-        'type': type(obj).__name__,
-        'attr': dir(obj),
+        'type': obj_type,
+        'attributes': attributes,
         'methods': methods,
-        'module': obj.__class__.__module__,
+        'module': obj_module
     }
 
-number_info = introspection_info(42)
+class MyTestClass:
+    def __init__(self):
+        self.attr1 = 'value1'
+        self.attr2 = 'value2'
+
+    def method1(self):
+        return 'method1 called'
+
+    def method2(self):
+        return 'method2 called'
+
+number_info = introspection_info(MyTestClass())
 pprint(number_info)
