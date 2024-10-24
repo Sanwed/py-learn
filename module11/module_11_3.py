d32964a4cb1f0c9a7d06a07a15987e36be3eb40d
Sanwed <kozyuckov.alex@gmail.com>
2024-10-24 19:39:35 +0400

    fix module_11_3 task

--- module11/module_11_3.py
+++ module11/module_11_3.py
@@ -5,7 +5,7 @@ def introspection_info(obj):
     obj_type = type(obj).__name__
     attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("__")]
     methods = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith("__")]
-    obj_module = obj.__module__
+    obj_module = obj.__class__.__module__
 
     return {
         'type': obj_type,
