9f6b19abf1478f5a0fcb63d79ad54256b1578798
Sanwed <kozyuckov.alex@gmail.com>
2024-10-24 19:34:58 +0400

    solve module_12_1 task

---
+++ module12/first/tests_12_1.py
@@ -0,0 +1,36 @@
+from module_12_1 import Runner
+import unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        """
+        Should be correct distance after runner walk
+        """
+        runner = Runner('Alex')
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        """
+        Should be correct distance after runner run
+        """
+        runner = Runner('Alex')
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        """
+        Should be not equal distance after challenge
+        walking and running runners
+        """
+        runner1 = Runner('Alex')
+        runner2 = Runner('Andrew')
+        for i in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+if __name__ == '__main__':
+    unittest.main()
