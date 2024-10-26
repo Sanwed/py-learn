0e17f1241bd7c568ab51a5186935f397ade33890
Sanwed <kozyuckov.alex@gmail.com>
2024-10-26 13:07:04 +0400

    solve module_12_2 task

---
+++ module12/tests_12_2.py
@@ -0,0 +1,71 @@
+import unittest
+
+class Runner:
+    def __init__(self, name, speed = 5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants[:]:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant.name
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = []
+
+    def setUp(self):
+        self.runner1 = Runner("Усэйн", speed = 10)
+        self.runner2 = Runner("Андрей", speed = 9)
+        self.runner3 = Runner("Ник", speed = 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for res in cls.all_results:
+            print('\n', res)
+
+    def test_race_1(self):
+        tournament = Tournament(90, self.runner1, self.runner3)
+        TournamentTest.all_results.append(tournament.start())
+        self.assertTrue(TournamentTest.all_results[-1][max(TournamentTest.all_results[-1].keys())], 'Ник')
+
+    def test_race_2(self):
+        tournament = Tournament(90, self.runner2, self.runner3)
+        TournamentTest.all_results.append(tournament.start())
+        self.assertTrue(TournamentTest.all_results[-1][max(TournamentTest.all_results[-1].keys())], 'Ник')
+
+    def test_race_3(self):
+        tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
+        TournamentTest.all_results.append(tournament.start())
+        self.assertTrue(TournamentTest.all_results[-1][max(TournamentTest.all_results[-1].keys())], 'Ник')
