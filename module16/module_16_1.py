af9f2ebf3d405ee4b1b3810869fbe03c56cc44ab
Sanwed <kozyuckov.alex@gmail.com>
2024-11-29 22:21:25 +0400

    solve module_16_1 task

--- module16/module_16_1.py
+++ module16/module_16_1.py
@@ -7,15 +7,17 @@ app = FastAPI()
 async def root():
   return {"message": "Главная страница"}
 
+
 @app.get("/user/admin")
 async def admin():
   return {"message": "Вы вошли как администратор"}
-
-@app.get("/user/{user_id}")
-async def user_more(user_id, name, age):
-  return {"message": f"Информация о пользователе. Имя: {name}, Возраст: {age}"}
 
 
 @app.get("/user/{user_id}")
 async def user(user_id):
   return {"message": f"Вы вошли как пользователь №{user_id}"}
+
+
+@app.get("/user")
+async def user_more(name, age):
+  return {"message": f"Информация о пользователе. Имя: {name}, Возраст: {age}"}
