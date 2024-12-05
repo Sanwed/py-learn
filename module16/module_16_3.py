6cf43e5badcf83f91e3e7a2261309b010dd7b9b0
Alexandr Kozyukov <kozyuckov.alex@gmail.com>
2024-12-05 15:20:10 +0400

    fix: add validation to requests

--- module16/module_16_3.py
+++ module16/module_16_3.py
@@ -1,4 +1,3 @@
-from os.path import defpath
 from typing import Annotated
 from fastapi import FastAPI, Path
 
@@ -15,14 +14,21 @@ async def get_users():
 
 
 @app.post("/user/{username}/{age}")
-async def create_user(username: str, age: int):
+async def create_user(
+    username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')],
+    age: Annotated[int, Path(ge=18, le=70, description="Enter age", example='24')]
+):
   max_index = str(max(map(int, users.keys())) + 1)
   users[max_index] = f"Имя: {username}, возраст: {age}"
   return f"User {max_index} is registered."
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str, username: str, age: int):
+async def update_user(
+    user_id: str,
+    username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')],
+    age: Annotated[int, Path(ge=18, le=70, description="Enter age", example='24')]
+):
   if user_id in users:
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'The user {user_id} is updated'
